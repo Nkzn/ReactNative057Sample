22ebea7aec720e9c0bc44d89193425bc23cf1202
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2018-08-30 15:26:37 +0900

    TypeScript works with non-configuration (only .tsx extension) :open_mouth:

--- App.tsx
+++ App.tsx
@@ -3,11 +3,11 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import MyText from "./MyText";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,13 +17,14 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <MyText text="This is typed text." />
       </View>
     );
   }
